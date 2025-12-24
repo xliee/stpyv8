3b063f41831a735c3270cd43fd6cf1e383c2b232
Angelo Dell'Aera <buffer@olografix.org>
2025-12-24 10:37:52 +0100

    Minor fix

--- settings.py
+++ settings.py
@@ -40,7 +40,6 @@ gn_args = {
 if platform.system() in ("Linux",):
     gn_args["v8_static_library"] = "true"
     gn_args["v8_monolithic_for_shared_library"] = "true"
-    gn_args["target_cpu"] = '"x64"'
 
 source_files = [
     "Exception.cpp",
