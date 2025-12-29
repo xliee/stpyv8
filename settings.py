dc95d720deae384a878a6260f3acb0ae33f9a803
Angelo Dell'Aera <buffer@olografix.org>
2025-12-29 12:35:51 +0100

    Minor changes

--- settings.py
+++ settings.py
@@ -156,8 +156,6 @@ elif os.name in ("posix",):
         libraries.append("rt")
         extra_compile_args.append("-std=c++2a")
         extra_compile_args.append("-Wno-comment")
-        extra_compile_args.append("-fuse-ld=lld")
-        extra_compile_args.append("-Wl,--compress-sections=.text=none")
     else:
         extra_compile_args.append("-std=c++20")
         extra_link_args.append("-headerpad_max_install_names")
